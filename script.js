437496da5c5f3f2b917f467330e8ce3229b7f022
Rhythm Shrimali <shrimalirhythm09@gmail.com>
2026-01-27 17:31:35 +0530

    initial commit for background color projects

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log("Rhythm")
+const parent = document.getElementById("parent");
+parent.addEventListener("click",(e) => {
+const child = e.target;
+const body = document.querySelector("body");
+body.style.backgroundColor = child.id;
+})
